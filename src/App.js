c43892e8b18d02fff5ebb7136a7fee1abc452561
sayma63 <saymasumi106353@gmail.com>
2022-07-11 19:26:44 +0600

    all routes set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Shared_pages/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Home/Contact';
+import About from './Pages/About';
+import Login from './Pages/Login';
+import Services from './Pages/Home/Services';
+import Loading from './Shared_pages/Loading';
+import NotFound from './Shared_pages/NotFound';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header></Header>
+     <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='home' element={<Home></Home>}></Route>
+      <Route path='contact' element={<Contact></Contact>}></Route>
+      <Route path='about' element={<About></About>}></Route>
+      <Route path='login' element={<Login></Login>}></Route>
+      <Route path='services' element={<Services></Services>}></Route>
+      <Route path='loading' element={<Loading></Loading>}></Route>
+      <Route path='*' element={<NotFound></NotFound>}></Route>
+     </Routes>
     </div>
   );
 }
