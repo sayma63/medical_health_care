c319649c0554cd0b34f9b7eea17a17bcd5b005b9
sayma63 <saymasumi106353@gmail.com>
2022-07-11 20:19:08 +0600

    loading added

--- src/Shared_pages/Loading.js
+++ src/Shared_pages/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Spinner} from 'react-bootstrap'
 
 const Loading = () => {
     return (
-        <div>
-            
+        <div className='p-5'>
+             <Spinner animation="border" variant="info" />
         </div>
     );
 };
