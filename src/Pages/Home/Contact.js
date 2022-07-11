47a608491b91d3e8a588ebc6c0ba97684c24abd5
sayma63 <saymasumi106353@gmail.com>
2022-07-11 21:54:49 +0600

    contact added

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import doctor from '../../Images/doctor.jpg'
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>This is contact</h1>
+        <div id='contact' className='container margin-top  '  >
+            <div className='row '>
+                <h1 className='text-center text-primary mb-4'><u>Contact With Doctor</u></h1>
+                <div className='col col-12 col-md-6 p-4 '>
+                       <h1 className='font-size1 text-info '>Chat <span className='text-success'> With Doctor</span></h1>
+                       <p className='fs-5'>Patients can easily interacting with doctor by the help of digital telecommunication.Patients can easily call a doctor to tell disease and get treatment from doctor.</p>
+                       <button className='btn btn-info fs-4 px-3'>Live</button>
+                </div>
+                <div className='col col-12 col-md-6 ' >
+                     <img className='image-fluid' src={doctor} height='300' style={{width:'100%'}} alt=""  />
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Contact from './Contact';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <h1 className='text-info text-center mt-2 '>Medical <span className='text-color'>Health Care</span></h1>
+            <Contact></Contact>
         </div>
     );
 };
