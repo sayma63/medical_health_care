b8f2b769e03b0595da0481d47b4dfa237d08c8c1
sayma63 <saymasumi106353@gmail.com>
2022-07-11 20:26:14 +0600

    not found page added

--- src/Shared_pages/NotFound.js
+++ src/Shared_pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='text-danger  text-center p-5'>
+            <h1 className='font-size'>This page is not available</h1>
+            <h1 className='font-size'>404</h1>
         </div>
     );
 };
