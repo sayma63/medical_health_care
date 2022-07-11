63b584dee97d0dd1b14c8e64b6e6cc4720b3ec50
sayma63 <saymasumi106353@gmail.com>
2022-07-11 19:55:36 +0600

    header added

--- src/Shared_pages/Header.js
+++ src/Shared_pages/Header.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Navbar,Nav,Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
 
 
 const Header = () => {
     return (
         <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg"  className='background-color'>
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand as={Link} to='/' href="#home"><span style={{color:'pink'}} className='fs-3'>Health</span> <span className='text-info fs-4'>Care</span></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link className='link-style' as={Link} to='services' href="home#services">Services</Nav.Link>
+      <Nav.Link className='link-style' as={Link} to='contact' href="home#contact">Contact</Nav.Link>
+      <Nav.Link  className='link-style' as={Link} to='blogs' href="blogs">Blogs</Nav.Link>
      
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
+      <Nav.Link className='link-style' as={Link} to='about' href="#about">AboutMe</Nav.Link>
+      <Nav.Link className='link-style' as={Link} to='login' eventKey={2} href="#login">
+        Login
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
