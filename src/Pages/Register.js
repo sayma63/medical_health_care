fd19cb84ceafdbbc9f97bb9d0be65810a9396d41
sayma63 <saymasumi106353@gmail.com>
2022-07-16 10:54:22 +0600

    register added

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Register.css'
+import { Link } from 'react-router-dom';
+import{useNavigate}from 'react-router-dom'
 
 const Register = () => {
+    const navigate=useNavigate()
+
+
+  const  navigateLogin=()=>{
+    navigate('/login')
+  }
+  const handleRegister=event=>{
+    event.preventDefault();
+  }
     return (
-        <div>
-            
+        <div className='register-form'>
+            <h2 className='text-center text-primary mt-2 mb-3'>Please Register</h2>
+            <form action="">
+                <input type='text' name='name' placeholder='Your name'></input>
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+                  
+                  <input type="password" name="password" id="" placeholder='Password' required/>
+                  <input type="submit" value="Register" />
+
+
+            </form>
+            <p >Already Have an Account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
