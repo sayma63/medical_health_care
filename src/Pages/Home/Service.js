d7b203714367a1eff2bc5fd2a6b798f750627771
sayma63 <saymasumi106353@gmail.com>
2022-07-27 10:45:56 +0600

    error checked

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -10,7 +10,7 @@ const Service = ({service}) => {
     }
     return (
         <div className='service '>
-            <img src={img} className=' img-fluid' style={{height:'300px'}} alt="" />
+            <img src={img} className='img-fluid' style={{height:'300px',width:'350px',padding:' 0px 1px'}} alt="" />
             <h1 className='text-success' >{name}</h1> 
             <h3>Price: ${price}</h3>
             <p className='px-2' >{description.length<60?description:description.slice(0,60)+'...read more' 
